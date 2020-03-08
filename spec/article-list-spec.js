aaf866e517720c5593cbfbfae993af7c14e1fdef
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-08 16:47:13 +0000

    Add addArcticle to ArcticleList object

--- spec/article-list-spec.js
+++ spec/article-list-spec.js
@@ -1,8 +1,21 @@
 'use strict';
 
 describe ('ArticleList', () => {
-  var list = new ArticleList()
+
+  var headline = 'headline'
+  var summary = 'summary'
+  var URL = 'URL'
+  var image = 'image'
+
+  var articleList = new ArticleList()
+
   it ('is created with an empty list array', () => {
-    expect(list.showArticles().length).toEq(0)
+    expect(articleList.showArticles().length).toEq(0)
   })
+
+  it ('creates and stores articles', function () {
+    articleList.addArticle(headline, summary, URL, image)
+    expect(articleList.showArticles().length).toEq(1)
+  })
+
 })
