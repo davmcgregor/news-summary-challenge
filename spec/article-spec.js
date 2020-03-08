5940afba49ef43baddec7df9166c4ad6c5f6c1ad
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-08 16:10:29 +0000

    Add image to article object

--- spec/article-spec.js
+++ spec/article-spec.js
@@ -13,4 +13,8 @@ describe('Article', () => {
   it('has a link to the full story', function () {
     expect(article.showURL()).toEq("https://www.theguardian.com/uk")
   })
+
+  it('has an image attached to the story', function () {
+    expect(article.showImage()).toEq("https://i.guim.co.uk/img/media/ad53a2b159050700ae0414f7c877634c6f9d394b/0_233_5202_3121/master/5202.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=e50b217fe53589727e9da28fd0609394")
+  })
 })
